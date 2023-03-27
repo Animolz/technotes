9578c60b878937f0c9c37d02075db512ee43b82a
lhn <namluong16@gmail.com>
2023-03-27 23:21:56 +0700

    ready to run v2

--- src/app/api/apiSlice.jsx
+++ src/app/api/apiSlice.jsx
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://hoangnam-api.onrender.com",
+  baseUrl: "https://technotes-backend-frzw.onrender.com",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
